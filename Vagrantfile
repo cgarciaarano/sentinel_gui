f89e3832f3e73416d39b2c271b6f113e186d724f
cgarciaarano <cgarciaarano@gmail.com>
2017-02-04 13:43:52 +0100

    Definition of DEV environment

---
+++ Vagrantfile
@@ -0,0 +1,19 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "williamyeh/ubuntu-trusty64-docker"
+  config.vm.provision "shell", path: "vagrant_provision.sh"
+  config.vm.network :forwarded_port, guest: 8080, host: 8080, id: 'web'
+  config.vm.network :forwarded_port, guest: 22, host: 2222, id: 'ssh'
+  config.vm.synced_folder ".", "/vagrant"
+  config.vm.hostname = "dev-sentinel"
+  config.vm.provider "virtualbox" do |vb| 
+     # Use VBoxManage to customize the VM. For example to change name, memory and DNS resolution:
+     vb.name = "devsentinel"
+     vb.customize ["modifyvm", :id, "--memory", "512"]
+	   vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+  end
+end
---
+++ vagrant_provision.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+HOME=/home/vagrant
+
+# Remove monitoring container
+docker stop cadvisor
+docker rm cadvisor
+docker rmi google/cadvisor
+docker rmi diogomonica/docker-bench-security
+docker rmi weaveworks/weavedb
+docker rmi swarm
+rm /etc/init/cadvisor.conf
+
+# Set commands after logon
+cat << EOF > $HOME/.profile
+. ~/.bashrc
+cd /vagrant
+while [ ! -e ~/.docker/config.json ] ; do
+	echo
+	echo
+	echo -e "\e[0;36mDocker Registry login\e[m"
+	echo -e "\e[0;33mdocker login \e[m"
+	echo
+	docker login
+done
+EOF
+
+# Aliases for docker compose
+cat << EOF > $HOME/.bash_aliases
+alias dev="docker-compose -f dockerfiles/dev/docker-compose.yml"
+alias python="dev exec web python"
+alias manage="python manage.py"
+alias shell="python manage.py shell"
+alias clean="docker images --no-trunc | grep '<none>' | awk '{ print \$3 }' | xargs -r docker rmi ; docker ps --filter status=dead --filter status=exited -aq  | xargs docker rm -v ; docker volume ls -qf dangling=true | xargs -r docker volume rm"
+alias bash="dev exec web /bin/sh"
+EOF
