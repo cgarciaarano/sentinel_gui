6d2c976556a536807a176401e163bfe73ba7fcea
cgarciaarano <cgarciaarano@gmail.com>
2017-02-22 09:23:37 +0100

    Added the first sentinel cluster to play with it

--- sentinel_gui/web.py
+++ sentinel_gui/web.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from sentinel_gui.core import models
+import os
+m = models.SentinelManager()
+m.add_sentinel_node(host=os.getenv('SENTINEL_SERVER', 'localhost'), port=os.getenv('SENTINEL_PORT', 26379))
 
 app = Flask(__name__)
-
-manager = models.SentinelManager()
 
 from sentinel_gui import views
