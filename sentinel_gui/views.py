bafb932bb46142465d3cb99d78deebbb2667fbce
cgarciaarano <cgarciaarano@gmail.com>
2017-03-05 22:00:25 +0100

    Added modal for adding sentinel

--- sentinel_gui/views.py
+++ sentinel_gui/views.py
@@ -1,18 +1,36 @@
 
 # std lib
+import logging
 
 # 3rd parties
-from flask import render_template
-#from flask import jsonify
+from flask import render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
 
 # local
 from sentinel_gui.web import app, sentinel_manager
 
+logger = logging.getLogger('sentinel_gui')
 
-@app.route('/')
-@app.route('/index')
+
+class AddNodeForm(FlaskForm):
+    fields = {"class": "form-control mb-2 mr-sm-2 mb-sm-0",
+              "placeholder": "sentinel_hostname",
+              }
+    host = StringField('Sentinel host', id='input_sentinel_host', validators=[DataRequired()], render_kw=fields)
+    port = IntegerField('Sentinel post', default=26379, validators=[DataRequired()], render_kw=fields)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Home')
+
+    form = AddNodeForm()
+    if form.validate_on_submit():
+        flash('Adding sentinel {} from user request'.format(form.host.data))
+        sentinel_manager.add_sentinel_node(host=form.host.data, port=form.port.data)
+    return render_template('index.html', form=form, title='Home')
 
 
 @app.route('/update', methods=['POST'])
--- sentinel_gui/web.py
+++ sentinel_gui/web.py
@@ -21,7 +21,9 @@ def setup_logging(app):
     app.logger.addHandler(lh)
 
 app = Flask(__name__)
+app.config.from_object('sentinel_gui.settings')
 setup_logging(app)
+
 
 sentinel_manager = models.SentinelManager()
 sentinel_manager.add_sentinel_node(host=os.getenv('SENTINEL_SERVER', 'localhost'), port=os.getenv('SENTINEL_PORT', 26379))
